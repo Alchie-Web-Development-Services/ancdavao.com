4ada1175bd6f03e6dbbb5f9a588c7c0d95d1ab28
Alchie Tagudin <chesteralantagudin@gmail.com>
2025-08-02 03:30:45 +0800

    feat: integrate Sanity CMS with GraphQL and React Query setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,18 @@ import "../src/styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import SEO from "@/components/SEO";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <SEO />
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </QueryClientProvider>
   );
 }
 
