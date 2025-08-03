5645ab0be4fc1bc9f677f332ef3694573ca5175d
Alchie Tagudin <chesteralantagudin@gmail.com>
2025-08-03 21:36:53 +0800

    feat: add About page sections with team and testimonials integration

--- codegen.ts
+++ codegen.ts
@@ -20,7 +20,6 @@ const config: CodegenConfig = {
         },
         "typescript",
         "typescript-operations",
-        "typescript-graphql-request",
       ],
       config: {
         rawRequest: true,
