65ab86622a03e809cd70d886dd3a344bc3fa7335
Alchie Tagudin <chesteralantagudin@gmail.com>
2025-09-09 16:58:16 +0800

    chore: update sitemap timestamps and add fields to Pledge interface

--- src/types/pledge.ts
+++ src/types/pledge.ts
@@ -1,5 +1,10 @@
+import { Timestamp } from "firebase/firestore";
+
 export interface Pledge {
+  id: string;
   amount: number;
   fulfilled: number;
   year: number;
+  paid_at: Timestamp;
+  payment_channel: string;
 }
